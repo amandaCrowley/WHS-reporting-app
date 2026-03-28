55845c0c77d3ac585b691718418b947097103e71
AmandaFoxley <amandacrowley56@gmail.com>
2026-03-28 12:56:48 +1100

    Connect database to node server

--- server/src/server.js
+++ server/src/server.js
@@ -1,21 +1,57 @@
 import express from 'express';
- 
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config(); //Load environment variables from .env
+
 const PORT = process.env.PORT || 8000; //Use the PORT environment variable if it's set, otherwise default to 8000
 const app = express();
 
 app.use(express.json()); //Tells our server to parse incoming JSON data in the request body and make it available under req.body
 
-function startServer() {
-    app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+const uri = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ac-gu3hwzr-shard-00-00.1tuxpwj.mongodb.net:27017,ac-gu3hwzr-shard-00-01.1tuxpwj.mongodb.net:27017,ac-gu3hwzr-shard-00-02.1tuxpwj.mongodb.net:27017/?ssl=true&replicaSet=atlas-x07b7f-shard-0&authSource=admin`;
+
+//Create a MongoClient as recommended on MongoDB website
+const DBclient = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
+async function startServer() {
+  try {
+    await DBclient.connect(); // Connect to MongoDB database
+    console.log("Connected to MongoDB");
+
+    // Get the database and attach to app.locals
+    const db = DBclient.db("WHS_App_DB");
+    app.locals.db = db;
+
+    // TESTING Print current users and issues ---
+    const users = await db.collection("User").find().toArray();
+    console.log("Users in DB:", users);
+
+    const issues = await db.collection("Issue").find().toArray();
+    console.log("Issues in DB:", issues);
+    // -----------------------------------
+
+    //Start Express server using app.listen
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+
+  } catch (e) {
+    console.error("Database connection failed:", e);
+    process.exit(1);
+  }
 }
 
-app.on("error", (error) => {
-  console.error("Server failed to start:", error);
-  process.exit(1);
-});
-
-//Start the server and listen on a port
+//Run method to start everything
 startServer();
+
+
+
+
 
